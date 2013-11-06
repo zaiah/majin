ad0b95d61c5bc7951bc1eac6704af34f6a7ccb4b
Antonio Collins <ramar.collins@gmail.com>
2013-11-05 20:52:30 -0500

    Added licenses, and regenerated a few library functions.

---
+++ lib/files/edit_in_place.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -
+#-----------------------------------------------------#
+# edit_in_place
+#
+# Opens a temporary file with variables within an editor.
+#-----------------------------------------------------#
---
+++ lib/random/randomize_to.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -
+#-----------------------------------------------------#
+# randomize_to
+#
+# Returns a string with characters in random order with length $1.
+#-----------------------------------------------------#
