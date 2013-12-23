f3c0be24df93e5f314d5d20f1a8e042bce5131be
Antonio Collins <ramar.collins@gmail.com>
2013-12-23 14:46:07 -0500

    Fun stuff

--- core/core-test.sh
+++ core/core-test.sh
@@ -10,6 +10,9 @@ PROGRAM="core-test"
 BINDIR="$(dirname "$(readlink -f $0)")"
 SELF="$(readlink -f $0)"
 
+# Libraries.
+source lib/__.sh
+
 # usage - Show usage message and die with $STATUS
 usage() {
    STATUS="${1:-0}"
@@ -28,6 +31,21 @@ usage() {
 }
 
 
+# stat - Checks the results of a pipe. 
+#
+# expect
+# - = /dev/stdin
+# 
+# Check if any args are flags.
+stat() {
+	local CMP=
+	[[ $2 == '-' ]] && CMP=$(< /dev/stdin)
+	[ $CMP == $1 ] && printf "%s\n" "Test succeeded!" || \
+		printf "%s\n" "Test failed." 
+
+	# Test can look to currently run line or be supplied with a name.
+}
+
 # Die if no arguments received.
 [ -z "$BASH_ARGV" ] && printf "Nothing to do\n" > /dev/stderr && usage 1
 
@@ -35,6 +53,12 @@ usage() {
 while [ $# -gt 0 ]
 do
    case "$1" in
+     -a|--all)
+         DO_OPTIONS=true
+         DO_TRAPS=true
+         DO_ERRORS=true
+         DO_DEFAULTS=true
+      ;;
      -o|--options)
          DO_OPTIONS=true
       ;;
@@ -66,23 +90,43 @@ done
 # options
 if [ ! -z $DO_OPTIONS ]
 then
-   printf '' > /dev/null
+	source opteval.sh
+
 fi
 
 # traps
 if [ ! -z $DO_TRAPS ]
 then
+	source traps.sh
    printf '' > /dev/null
 fi
 
 # defaults
 if [ ! -z $DO_DEFAULTS ]
 then
+	source files.sh
    printf '' > /dev/null
 fi
 
 # errors
 if [ ! -z $DO_ERRORS ]
 then
+	source error.sh
+
+	# Create a sink.
+	tmp_file -n ERRLOG
+
+	# Log a message.
+	error -m "Error occurred here." 2>$ERRLOG
+
+	# Log a message to file.
+	error -m "Error occurred here." --log $ERRLOG
+
+	# Get line count.  stat $1 $2
+	# stat checks that an output is what its supposed to be.
+	# $1 = $2 is good.  else is not...
+	cat $ERRLOG | wc -l | stat 2 -
+	tmp_file -l 
+
    printf '' > /dev/null
 fi
--- core/files.sh
+++ core/files.sh
@@ -1,10 +1,97 @@
-#------------------------------------------------------
-# default_file_check 
-# 
-# Something can check for all these to see if they
-# exist.  The absolute filepath will not always be
-# the same, possibly.
 #-----------------------------------------------------#
-STDERR="/dev/stderr"
-STDOUT="/dev/stdout"
-STDIN="/dev/stdin"
+# dfc
+#
+# Does some quick checks for common file descriptors.
+#-----------------------------------------------------#
+dfc() {
+	LIBPROGRAM="dfc"
+	# Static variables
+	STDERR=
+	STDOUT=
+	STDIN=
+	
+	# dfc_usage - Show usage message and die with $STATUS
+	dfc_usage() {
+	   STATUS="${1:-0}"
+	   echo "Usage: ./$LIBPROGRAM
+		[ -  ]
+	
+	-l | --list                   Return a list of valid file descriptors. 
+	-e | --stderr                 Return the first valid file descriptor 
+                                 for standard error.
+	-o | --stdout                 Return the first valid file descriptor 
+                                 for standard output. 
+	-i | --stdin                  Return the first valid file descriptor
+                                 for standard input. 
+	-i | --integer                Return the integer pertaining to the above
+                                 options. 
+	-v | --verbose                Be verbose in output.
+	-h | --help                   Show this help and quit.
+	"
+	   exit $STATUS
+	}
+	
+	
+	# Die if no arguments received.
+	[ -z "$#" ] && printf "Nothing to do\n" > /dev/stderr && dfc_usage 1
+	
+	# Process options.
+	while [ $# -gt 0 ]
+	do
+	   case "$1" in
+	     -l|--list)
+	         DO_LIST=true
+	      ;;
+	     -e|--stderr)
+	         DO_STDERR=true
+	      ;;
+	     -o|--stdout)
+	         DO_STDOUT=true
+	      ;;
+	     -s|--stdin)
+	         DO_STDIN=true
+	      ;;
+	     -i|--integer)
+	         DO_INTEGER=true
+	      ;;
+	     -v|--verbose)
+	        VERBOSE=true
+	      ;;
+	     -h|--help)
+	        dfc_usage 0
+	      ;;
+	     --) break;;
+	     -*)
+	      printf "Unknown argument received.\n" > /dev/stderr;
+	      dfc_usage 1
+	     ;;
+	     *) break;;
+	   esac
+	shift
+	done
+	
+	# list
+	[ ! -z $DO_LIST ] && {
+	   printf '' > /dev/null
+	}
+	
+	# stderr
+	[ ! -z $DO_STDERR ] && {
+	   printf '' > /dev/null
+	}
+	
+	# stdout
+	[ ! -z $DO_STDOUT ] && {
+	   printf '' > /dev/null
+	}
+	
+	# stdin
+	[ ! -z $DO_STDIN ] && {
+	   printf '' > /dev/null
+	}
+	
+	# integer
+	[ ! -z $DO_INTEGER ] && {
+	   printf '' > /dev/null
+	}
+}
